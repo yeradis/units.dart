893f30e65522b45f35266f8f9b4da19a07ec6040
Yeradis P. Barbosa Marrero <yeradis@gmail.com>
2017-10-13 07:23:08 +0200

    Refactor variable names

--- lib/src/speed.dart
+++ lib/src/speed.dart
@@ -1,12 +1,14 @@
+import 'dart:core';
+
 class Speed implements Comparable<Speed> {
     ///meters per second to:
-    static const double _msec_to_kilometers_per_hour = 3.6;
-    static const double _msec_to_mile_per_hour = 2.23693629;
+    static const double _msec_to_kmh = 3.6;
+    static const double _msec_to_mih = 2.23693629;
     static const double _msec_to_knot = 1.94384449;
 
     ///other measurements to meters per second
     static const double _kmh_to_msec = 0.27777777777778;
-    static const double _mph_to_msec = 0.44704;
+    static const double _mih_to_msec = 0.44704;
     static const double _knot_to_msec = 0.514444444;
 
     /*
@@ -16,12 +18,12 @@ class Speed implements Comparable<Speed> {
 
     const Speed.fromMeterPerSecond({double value : 0.0}) : _speed = value;
     const Speed.fromKilometerPerHour({double value : 0.0}) : _speed = _kmh_to_msec * value;
-    const Speed.fromMilePerHour({double value: 0.0}): _speed = _mph_to_msec * value;
+    const Speed.fromMilePerHour({double value: 0.0}): _speed = _mih_to_msec * value;
     const Speed.fromKnot({double value: 0.0}): _speed = _knot_to_msec * value;
 
     double get inMeterPerSecond => _speed > 0.0 ? _speed : 0.0;
-    double get inKilometerPerHour => _speed > 0.0 ? _msec_to_kilometers_per_hour * _speed : 0.0;
-    double get inMilePerHour => _speed > 0.0 ? _msec_to_mile_per_hour * _speed : 0.0;
+    double get inKilometerPerHour => _speed > 0.0 ? _msec_to_kmh * _speed : 0.0;
+    double get inMilePerHour => _speed > 0.0 ? _msec_to_mih * _speed : 0.0;
     double get inKnot => _speed > 0.0 ? _msec_to_knot * _speed : 0.0;
 
     /**
