fdb545a422f2cbfc9ce35a9015564bf24a6ff581
Yeradis P. Barbosa Marrero <yeradis@gmail.com>
2017-10-13 11:25:27 +0200

    Add new

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,3 +15,7 @@ Minor improvements to Speed and Length, also added some tests
 
 Now, all numbers are forced to a max representation of exactly 6 significant digits
 For example: 12345678910.12345678 will turn into: 12345678910.123457
+
+## 0.0.3
+
+Minor update, add new tests
--- test/speed_test.dart
+++ test/speed_test.dart
@@ -2,37 +2,70 @@ import 'package:units/units.dart';
 import 'package:test/test.dart';
 
 void main() {
-    group('Meters per seconds tests', () {
+    group('Speed tests', () {
         test('Having -1.0 should return 0.0', () {
             expect(new Speed.fromMeterPerSecond(value: -1.0).inMeterPerSecond, 0.0);
+            expect(new Speed.fromKilometerPerHour(value: -1.0).inMeterPerSecond, 0.0);
+            expect(new Speed.fromMilePerHour(value: -1.0).inMeterPerSecond, 0.0);
+            expect(new Speed.fromKnot(value: -1.0).inMeterPerSecond, 0.0);
         });
 
         test('Having 0.0 should return 0.0', () {
             expect(new Speed.fromMeterPerSecond(value: 0.0).inMeterPerSecond, 0.0);
+            expect(new Speed.fromKilometerPerHour(value: 0.0).inMeterPerSecond, 0.0);
+            expect(new Speed.fromMilePerHour(value: 0.0).inMeterPerSecond, 0.0);
+            expect(new Speed.fromKnot(value: 0.0).inMeterPerSecond, 0.0);
+
         });
 
-        test('Having 1.0 should return 1.0', () {
+        test('Having 1m/s should match return', () {
             expect(new Speed.fromMeterPerSecond(value: 1.0).inMeterPerSecond, 1.0);
+            expect(new Speed.fromMeterPerSecond(value: 1.0).inKilometerPerHour, 3.6);
+            expect(new Speed.fromMeterPerSecond(value: 1.0).inMilePerHour, 2.236936);
+            expect(new Speed.fromMeterPerSecond(value: 1.0).inKnot, 1.943844);
+        });
+
+        test('Having 1km/h should match return', () {
+            expect(new Speed.fromKilometerPerHour(value: 1.0).inKilometerPerHour, 1.0);
+            expect(new Speed.fromKilometerPerHour(value: 1.0).inMeterPerSecond, 0.277778);
+            expect(new Speed.fromKilometerPerHour(value: 1.0).inMilePerHour, 0.621371);
+            expect(new Speed.fromKilometerPerHour(value: 1.0).inKnot, 0.539957);
+        });
+
+        test('Having 1mi/h should match return', () {
+            expect(new Speed.fromMilePerHour(value: 1.0).inKilometerPerHour, 1.609344);
+            expect(new Speed.fromMilePerHour(value: 1.0).inMeterPerSecond, 0.44704);
+            expect(new Speed.fromMilePerHour(value: 1.0).inMilePerHour, 1.0);
+            expect(new Speed.fromMilePerHour(value: 1.0).inKnot, 0.868976);
+        });
+
+        test('Having 1kn (knot) should match return', () {
+            expect(new Speed.fromKnot(value: 1.0).inKilometerPerHour, 1.852);
+            expect(new Speed.fromKnot(value: 1.0).inMeterPerSecond, 0.514444);
+            expect(new Speed.fromKnot(value: 1.0).inMilePerHour, 1.150779);
+            expect(new Speed.fromKnot(value: 1.0).inKnot, 1.0);
         });
     });
 
-    group('Kilometers per hour tests', () {
-        test('Having -1.0 should return 0.0', () {
-            expect(new Speed.fromKilometerPerHour(value: -1.0).inMeterPerSecond, 0.0);
+    group('Speed comparisson tests', () {
+        Speed speed20;
+        Speed speed4;
+
+        setUp(() {
+            speed20 = new Speed.fromMeterPerSecond(value: 20.0);
+            speed4 = new Speed.fromMeterPerSecond(value: 4.0);
         });
 
-        test('Having 0.0 should return 0.0', () {
-            expect(new Speed.fromKilometerPerHour(value: 0.0).inMeterPerSecond, 0.0);
+        test('Comparing same speed, should return 0 (same speed)', () {
+            expect(speed20.compareTo(speed20), equals(0));
         });
 
-        test('Having 1.0 should return 0.27', () {
-            expect(new Speed.fromKilometerPerHour(value: 1.0).inMeterPerSecond, 0.277778);
+        test('Comparing speed, Speed 4 is shorter than Speed 20', () {
+            expect(speed4.compareTo(speed20), lessThan(0));
         });
 
-        test('Having 360 should return 100', () {
-            expect(new Speed.fromKilometerPerHour(value: 360.0).inMeterPerSecond, allOf(
-                greaterThanOrEqualTo(100), lessThan(101)
-            ),);
+        test('Comparing speed, speed 20 is greater than speed 4', () {
+            expect(speed20.compareTo(speed4), greaterThan(0));
         });
     });
 }
