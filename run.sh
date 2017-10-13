e7270b026c85dfda476120aefea253352c4b1147
Yeradis P. Barbosa Marrero <yeradis@gmail.com>
2017-10-13 20:03:25 +0200

    Updated helper script for local work

--- run.sh
+++ run.sh
@@ -19,15 +19,55 @@ if [[ $# -eq 0 ]] ; then
     usage;
 fi
 
-echo "If packages missing, check the .travil.yaml, there appears the activation commands"
+run_setupIfNeeded(){
+    if [ ! -d "packages" ]; then
+        echo "Missing config... setting up now!"
+        echo -e "\n\n"
+        pub get --packages-dir
+        pub global activate coverage
+        pub global activate coveralls
+        echo -e "\n\n"
+    fi
+}
+
+run_setupIfNeeded
+
+run_tests(){
+    dartanalyzer .
+    pub run test -r expanded test/*.dart
+}
+
+run_coverage_generator(){
+    run_setupIfNeeded
+    pub run dart_codecov_generator --report-on=lib/ --verbose
+}
+
+run_coverage_open(){
+    run_setupIfNeeded
+
+    if [ ! -f "coverage_report/index.html" ]; then
+        echo "Coverage report is missing... generating now now!"
+        echo -e "\n\n"
+        pub get --packages-dir
+        pub global activate coverage
+        pub global activate coveralls
+        echo -e "\n\n"
+    fi
+
+    open coverage_report/index.html
+}
+
+run_coverage_upload(){
+    pub global run coveralls coverage.lcov
+}
 
 for var in "$@"
 do
     case "${var}"
     in
-        --tests) pub run test -r expanded test/*.dart;;
-        --coverage) pub run dart_codecov_generator --report-on=lib/ --verbose;;
-        --coverage_open) open coverage_report/index.html;;
-        --coverage_upload) pub global run coveralls coverage.lcov;;
+        --tests) run_tests;;
+        --coverage) run_coverage_generator;;
+        --coverage_open) run_coverage_open;;
+        --coverage_upload) run_coverage_upload;;
     esac
 done
